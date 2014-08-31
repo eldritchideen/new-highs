fa1d7366f4ff2bd9dce1dca6b3527f5011ce2604
Steven Cook <steve.j.cook@gmail.com>
2014-08-31 14:32:31 +1000

    Update project details

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject new-highs "0.1.0-SNAPSHOT"
   :description "Keep track of shares on the ASX that are making new yearly highs."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/eldritchideen/new-highs"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
